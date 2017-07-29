b7d5144afabba6208efc8c16c19e263e739dda0f
Chulong Li <chulong1028@gmail.com>
2017-07-29 19:10:46 +0800

    Create 278. First Bad Version.java

---
+++ 278. First Bad Version.java
@@ -0,0 +1,17 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int low = 1, high = n, mid;
+        while (low < high) {
+            mid = low + (high - low) / 2;
+            if (isBadVersion(mid)) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+}
