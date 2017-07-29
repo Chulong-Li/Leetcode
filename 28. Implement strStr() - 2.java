df2eb852dd4ddc83eb7ab180b0d5fe00ac05d767
Chulong Li <chulong1028@gmail.com>
2017-07-29 17:19:12 +0800

    Create 28. Implement strStr() - 2.java

---
+++ 28. Implement strStr() - 2.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int strStr(String haystack, String needle) {  
+        for (int i = 0; i < haystack.length() - needle.length() + 1; i++) {
+            int j = 0;
+            for (; j < needle.length(); j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) break;
+            }                
+            if (j == needle.length()) return i;
+        }
+        return -1;
+    }
+}
