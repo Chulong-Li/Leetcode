e8d2a3b86456df6c683f0e0ef38469e2d622b712
Chulong Li <chulong1028@gmail.com>
2017-07-29 17:28:41 +0800

    Create 28. Implement strStr() - 3.java

---
+++ 28. Implement strStr() - 3.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        for (int i = 0; ; i++) {
+            for (int j = 0; ; j++) {
+                if (j == needle.length()) retur i;
+                if (i + j ==haystack.length()) return -1;
+                if (hackstack.charAt(i + j) != needle.charAt(j)) break; 
+            }
+        }
+    }
+}
