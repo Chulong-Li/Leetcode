e11ac10674512da4bed97a26c2561c82c9e81375
Chulong Li <chulong1028@gmail.com>
2017-08-01 18:09:01 +0800

    Create 387. First Unique Character in a String.java

---
+++ 387. First Unique Character in a String.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int firstUniqChar(String s) {
+        int[] hashTable = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            hashTable[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (hashTable[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
